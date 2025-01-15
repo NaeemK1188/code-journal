19efd2942881f76246c0332d11bfae4bf89267c6
NaeemK1188 <naeemalbirkdar@aol.com>
2025-01-15 09:02:59 +0000

    adding directions for adding image

--- ts/main.ts
+++ ts/main.ts
@@ -14,7 +14,11 @@ $inputURL.addEventListener('input', (event: Event) => {
   console.log('input event was fired');
   const eventTarget = event.target as HTMLInputElement;
   // $imgURL = $inputURL.textContent;
+  // here we are only updating the variable $imgsrc not the actual image
   $imgURL = eventTarget.value;
+  // here we are explicitly updating the image by using new src variable $imgURL
+  // it worked although the conversion from HTMLimageElement to string
+  // $img = $imgURL;
   $img.setAttribute('src', $imgURL);
   console.log($imgURL);
 });
