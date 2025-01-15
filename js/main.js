2082a15949ddabf391a59091f83f0ba6b93f786b
NaeemK1188 <naeemalbirkdar@aol.com>
2025-01-15 17:50:25 +0000

    adding event listener to  and prevent it from refreshing

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
 'use strict';
 const $inputURL = document.querySelector('.inputURL');
-// console.log($input1);
 const $img = document.querySelector('img');
 // console.log($img?.getAttribute('src'));
-if (!$inputURL || !$img) {
-  throw new Error('$inputURL or $img are not exists');
+const $form = document.querySelector('form');
+// console.log($form);
+if (!$inputURL || !$img || !$form) {
+  throw new Error('$inputURL or $img or $form are not exists');
 }
-let $imgURL = $img.getAttribute('src');
-// console.log($imgURL);
 $inputURL.addEventListener('input', (event) => {
-  console.log('input event was fired');
+  console.log('input event is fired');
   const eventTarget = event.target;
   // $imgURL = $inputURL.textContent;
-  $imgURL = eventTarget.value;
-  $img.setAttribute('src', $imgURL);
+  // here we are only updating the variable $imgURL not the actual image
+  const $imgURL = eventTarget.value; // value of the input like .textContent
   console.log($imgURL);
+  // here we are explicitly updating the image by using new src variable $imgURL
+  // it worked although the conversion from HTMLimageElement to string
+  // $img = $imgURL;
+  $img.setAttribute('src', $imgURL);
+  // console.log($imgURL);
+});
+$form.addEventListener('submit', (event) => {
+  console.log('submit event is fired');
+  event.preventDefault();
+  console.log('form submitted');
 });
--- ts/main.ts
+++ ts/main.ts
@@ -1,24 +1,31 @@
 const $inputURL = document.querySelector('.inputURL');
-// console.log($input1);
+
 const $img = document.querySelector('img');
 // console.log($img?.getAttribute('src'));
+const $form = document.querySelector('form');
+// console.log($form);
 
-if (!$inputURL || !$img) {
-  throw new Error('$inputURL or $img are not exists');
+if (!$inputURL || !$img || !$form) {
+  throw new Error('$inputURL or $img or $form are not exists');
 }
 
-let $imgURL = $img.getAttribute('src');
-// console.log($imgURL);
-
 $inputURL.addEventListener('input', (event: Event) => {
-  console.log('input event was fired');
+  console.log('input event is fired');
   const eventTarget = event.target as HTMLInputElement;
+
   // $imgURL = $inputURL.textContent;
-  // here we are only updating the variable $imgsrc not the actual image
-  $imgURL = eventTarget.value;
+  // here we are only updating the variable $imgURL not the actual image
+  const $imgURL = eventTarget.value; // value of the input like .textContent
+  console.log($imgURL);
   // here we are explicitly updating the image by using new src variable $imgURL
   // it worked although the conversion from HTMLimageElement to string
   // $img = $imgURL;
   $img.setAttribute('src', $imgURL);
-  console.log($imgURL);
+  // console.log($imgURL);
+});
+
+$form.addEventListener('submit', (event: Event) => {
+  console.log('submit event is fired');
+  event.preventDefault();
+  console.log('form submitted');
 });
