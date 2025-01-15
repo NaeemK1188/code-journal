5e1519a1d5386d33784ca3f977dc77be6abdb1da
NaeemK1188 <naeemalbirkdar@aol.com>
2025-01-15 08:42:11 +0000

    adding image functionality #1

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
+'use strict';
 const $inputURL = document.querySelector('.inputURL');
 // console.log($input1);
 const $img = document.querySelector('img');
 // console.log($img?.getAttribute('src'));
-
 if (!$inputURL || !$img) {
   throw new Error('$inputURL or $img are not exists');
 }
-
 let $imgURL = $img.getAttribute('src');
 // console.log($imgURL);
-
-$inputURL.addEventListener('input', (event: Event) => {
+$inputURL.addEventListener('input', (event) => {
   console.log('input event was fired');
-  const eventTarget = event.target as HTMLInputElement;
+  const eventTarget = event.target;
   // $imgURL = $inputURL.textContent;
   $imgURL = eventTarget.value;
   $img.setAttribute('src', $imgURL);
