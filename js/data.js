5281a42e553781cbc7a07532a0f30e7db490b05a
NaeemK1188 <naeemalbirkdar@aol.com>
2025-01-16 04:54:04 +0000

    adding function write() to local storage and  read() to read from storage #1

--- js/data.js
+++ js/data.js
@@ -1,11 +1,22 @@
 "use strict";
 const data = {
     view: 'entry-form',
-    entries: [],
+    entries: [], // array of objects entry
     editing: null,
     nextEntryId: 1,
 };
 function writeEntry() {
     const entryJSON = JSON.stringify(data.entries);
     localStorage.setItem('entry-storage', entryJSON);
+}
+function readEntry() {
+    const entryList = localStorage.getItem('entry-storage');
+    // or if (entry != null)
+    if (entryList) {
+        // or const newEntryList:Data[] = JSON.parse(entryList);
+        return JSON.parse(entryList);
+    }
+    else {
+        return [];
+    }
 }
--- ts/data.ts
+++ ts/data.ts
@@ -9,16 +9,34 @@ interface Data
 
 const data:Data = {
   view: 'entry-form',
-  entries: [],
+  entries: [], // array of objects entry
   editing: null,
   nextEntryId: 1,
 };
 
 
 
-// function writeEntry():void
-// {
-//   const entryJSON = JSON.stringify(data.entries);
-//   localStorage.setItem('entry-storage',entryJSON);
+function writeEntry():void
+{
+  const entryJSON = JSON.stringify(data.entries);
+  localStorage.setItem('entry-storage', entryJSON);
 
-// }
+}
+
+function readEntry():Data[]
+{
+  const entryList = localStorage.getItem('entry-storage');
+
+  // or if (entry != null)
+  if(entryList)
+  {
+    // or const newEntryList:Data[] = JSON.parse(entryList);
+    return JSON.parse(entryList) as Data[];
+  }
+
+  else
+  {
+    return [];
+  }
+
+}
