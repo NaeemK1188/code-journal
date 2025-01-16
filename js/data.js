fb76c5b2c99ecc733cd64c50acdce6462f4404a4
NaeemK1188 <naeemalbirkdar@aol.com>
2025-01-16 18:47:28 +0000

    updating write() and read() to return the whole object not just the Entry[]

--- js/data.js
+++ js/data.js
@@ -1,21 +1,23 @@
 "use strict";
-const data = {
-    view: 'entry-form',
-    entries: [],
-    editing: null,
-    nextEntryId: 1,
-};
-data.entries = readEntry();
+// calling function definition readEntry()
+const data = readEntry();
 function writeEntry() {
-    const entryJSON = JSON.stringify(data.entries);
+    // we should stringify the whole object not only data entry
+    // now we can see all the object data are stringified in the localstorage not only the Entry[]
+    const entryJSON = JSON.stringify(data);
     localStorage.setItem('entry-storage', entryJSON);
 }
 function readEntry() {
     const entryList = localStorage.getItem('entry-storage');
     if (entryList) {
+        // we don't want to return and array of entry only, it has to be the entire object Data
+        // if there is a data in localstorage we parse it
+        // return the whole object not just the entry array
         return JSON.parse(entryList);
     }
     else {
-        return [];
+        // if its empty we are returning the default state of the object not only the
+        // Entry[] because the return is Data object with the initial state
+        return { view: 'entry-form', entries: [], editing: null, nextEntryId: 1 };
     }
 }
--- ts/data.ts
+++ ts/data.ts
@@ -1,3 +1,4 @@
+// out data model in local storage
 interface Data
 {
   view: string;
@@ -6,39 +7,42 @@ interface Data
   nextEntryId:number;
 }
 
-const data:Data = {
-  view: 'entry-form',
-  entries: [],
-  editing: null,
-  nextEntryId: 1,
-};
 
 
-data.entries = readEntry();
 
+// calling function definition readEntry()
+const data = readEntry();
 
 function writeEntry():void
 {
-  const entryJSON = JSON.stringify(data.entries);
+  // we should stringify the whole object not only data entry
+  // now we can see all the object data are stringified in the localstorage not only the Entry[]
+  const entryJSON = JSON.stringify(data);
   localStorage.setItem('entry-storage', entryJSON);
 
 }
 
 
-function readEntry():Entry[]
+function readEntry(): Data
 {
   const entryList = localStorage.getItem('entry-storage');
 
 
   if(entryList)
   {
-   
-    return JSON.parse(entryList) as Entry[];
+    // we don't want to return and array of entry only, it has to be the entire object Data
+    // if there is a data in localstorage we parse it
+    // return the whole object not just the entry array
+    return JSON.parse(entryList) as Data;
   }
 
   else
   {
-    return [];
+
+     // if its empty we are returning the default state of the object not only the
+     // Entry[] because the return is Data object with the initial state
+    return   { view: 'entry-form', entries: [], editing: null, nextEntryId: 1};
+
   }
 
 }
